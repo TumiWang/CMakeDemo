e126a9904ac0af00953e9e99581329f2d74dde2c
Tumi Wang <lianyuguhun@gmail.com>
2025-07-31 16:37:58 +0800

    demo4

--- demo4/src/name.cpp
+++ demo4/src/name.cpp
@@ -2,6 +2,13 @@
 
 #include <string>
 
+// 添加下面这段
+// 含义是，如果定义了宏 CPP_FANTASY_CHINESE
+// 则 取消这个宏的定义 -- 相当于未定义
+#ifdef CPP_FANTASY_CHINESE
+#undef CPP_FANTASY_CHINESE
+#endif
+
 std::string GetName() {
 #ifdef CPP_FANTASY_CHINESE
     return "C++幻想";
