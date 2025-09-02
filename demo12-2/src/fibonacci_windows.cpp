e2c9e65efb48770928162bd6ee7134d73d871cce
Tie.Wang <tie.wang@cyclone-robotics.com>
2025-09-02 10:38:33 +0800

    编译问题

--- demo12-2/src/fibonacci_windows.cpp
+++ demo12-2/src/fibonacci_windows.cpp
@@ -8,7 +8,7 @@
 #include <Windows.h>
 
 namespace {
-const char library_filename[] = "../demo11-1/build/Release/info.dll";
+const char library_filename[] = "../demo12-1/build/Release/info.dll";
 const char fibonacci_func_name[] = "CreateFibonacciInstance";
 
 using fibonacci_func_type = FibonacciInterface*(*)();
@@ -19,7 +19,7 @@ FibonacciInterface* CreateFibonacciInterface() {
         HMODULE info_module = LoadLibraryA(library_filename);
         if (!info_module) break;
         fibonacci_func_type fibonacci_func = 
-            reinterpret_cast<fibonacci_func_type>(GetProcAddress(info_module, fibonacci_func_name););
+            reinterpret_cast<fibonacci_func_type>(GetProcAddress(info_module, fibonacci_func_name));
         if (!fibonacci_func) break;
         return fibonacci_func();
     } while(false);
