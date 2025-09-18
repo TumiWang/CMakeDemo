21a587a8d33ade0db96f5d8e0388c3a1d0e11439
tumi <lianyuguhun@gmail.com>
2025-09-18 17:13:59 +0800

    demo14

---
+++ demo14-1/include/info.h
@@ -0,0 +1,47 @@
+// 文件 include/info.h
+
+#ifndef __INFO_H___
+#define __INFO_H___
+
+// 实际上，宏通常都不缩进的，这里为了看看清楚
+#ifdef _WIN32
+    // Windows
+    #ifdef INFO_IMPL
+        #define MY_INFO_EXPORT __declspec(dllexport)
+    #else
+        #define MY_INFO_EXPORT __declspec(dllimport)
+    #endif // INFO_IMPL
+#else
+    #if __has_attribute(visibility)
+        #define MY_INFO_EXPORT __attribute__((visibility("default")))
+    #endif
+#endif // _WIN32
+
+#ifndef MY_INFO_EXPORT
+    #define MY_INFO_EXPORT
+#endif
+
+class FibonacciInterface {
+public:
+    FibonacciInterface() = default;
+    virtual ~FibonacciInterface() = default;
+
+    virtual const char* GetValue(unsigned int index) = 0;
+
+    // 这是自清理方法
+    // 该方法调用后，这个接口指针就失效了
+    virtual void Cleanup() = 0;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 如果使用c++编译器，该函数会使用C风格
+MY_INFO_EXPORT FibonacciInterface* CreateFibonacciInstance(bool first_is_zero);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __INFO_H___
