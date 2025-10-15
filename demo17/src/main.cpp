4c6e2cce4ee8abb3ff5a6993a91f83bd873b6d0c
tumi <lianyuguhun@gmail.com>
2025-10-15 14:04:27 +0800

    demo17

--- demo17/src/main.cpp
+++ demo17/src/main.cpp
@@ -5,6 +5,6 @@ int main()
 #ifdef CURRENT_TYPE
     std::cout << "宏CURRENT_TYPE -- " << CURRENT_TYPE << std::endl;
 #else
-    std::cout << "为定义宏 -- CURRENT_TYPE" << std::endl;
+    std::cout << "未定义宏 -- CURRENT_TYPE" << std::endl;
 #endif
 }
