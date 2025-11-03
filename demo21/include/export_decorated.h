862de13cc46a31a48ba0a271c883ed7fdc70aed8
tumi <lianyuguhun@gmail.com>
2025-11-03 14:46:16 +0800

    demo21

---
+++ demo21/include/export_decorated.h
@@ -0,0 +1,12 @@
+// 文件 include/export_decorated.h
+
+#ifndef __EXPORT_DECORATED_H_
+
+// linux下动态库 导出函数的修饰词
+#if __has_attribute(visibility)
+    #define EXPORT_DECORATED __attribute__((visibility("default")))
+#else
+    #define EXPORT_DECORATED
+#endif
+
+#endif // @EXPORT_DECORATED@
---
+++ demo21/info/info.cpp
@@ -0,0 +1,8 @@
+// 文件 info/info.cpp
+
+#include "info.h"
+
+const char* GetName()
+{
+    return "C++幻想";
+}
