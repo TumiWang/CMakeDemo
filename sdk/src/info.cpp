74f687e749a6fcf507c319b6e67a37b22100c9cd
tumi <lianyuguhun@gmail.com>
2025-11-27 14:35:51 +0800

    修改sdk

--- sdk/src/info.cpp
+++ sdk/src/info.cpp
@@ -6,8 +6,19 @@
 
 namespace {
     std::once_flag init_arch_;
-
     std::string arch_;
+
+    std::once_flag init_lib_prefix_;
+    std::vector<std::string> lib_prefix_;
+
+    void InitLibPrefix() {
+        lib_prefix_.push_back("cmake");
+        lib_prefix_.push_back("gcc");
+        lib_prefix_.push_back("pkgconfig");
+        lib_prefix_.push_back(GetArch());
+        lib_prefix_.push_back("clang");
+        lib_prefix_.push_back("llvm");
+    }
 }
 
 const std::string& GetArch() {
@@ -18,4 +29,26 @@ const std::string& GetArch() {
         }
     });
     return arch_;
+}
+
+bool MatchLibPrefix(const std::string& name) {
+    std::call_once(init_lib_prefix_, InitLibPrefix);
+
+    bool result = false;
+
+    for (auto& item: lib_prefix_) {
+        if (name.size() < item.size()) continue;
+        if (name.size() == item.size()) {
+            if (item != name) continue;
+            result = true;
+            break;
+        }
+        auto temp = name.substr(0, item.size());
+        if (temp == item) {
+            result = true;
+            break;
+        }
+    }
+
+    return result;
 }
--- sdk/src/info.h
+++ sdk/src/info.h
@@ -2,7 +2,9 @@
 #define __INFO_H_
 
 #include <string>
+#include <vector>
 
 const std::string& GetArch();
+bool MatchLibPrefix(const std::string& name);
 
 #endif // __INFO_H_
