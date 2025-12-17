8a3e584bf0ecec21548fcba50ab3bc7dfeab7b41
Tumi Wang <lianyuguhun@gmail.com>
2025-12-17 20:38:35 +0800

    修改交叉编译的错误

--- cross/toolchain/ubuntu20_04-aarch64.cmake
+++ cross/toolchain/ubuntu20_04-aarch64.cmake
@@ -1,6 +1,6 @@
 # 交叉编译
-set(CMAKE_HOST_SYSTEM_NAME Linux)
-set(CMAKE_HOST_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(TARGET_TRIPLE aarch64-linux-gnueabihf)
 
 # 指定使用Ninja
