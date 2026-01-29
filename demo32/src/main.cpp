56916ddd1dccfd665ab9b0ca609261020edd9398
Tumi Wang <lianyuguhun@gmail.com>
2026-01-30 07:18:23 +0800

    demo32

--- demo32/src/main.cpp
+++ demo32/src/main.cpp
@@ -7,12 +7,14 @@ int main()
 {
     int* p = new int[5];
 
-    // 没有下面这句, p[3] 实际上没有初始化
     // memset(p, 0, sizeof(*p) * 5);
+    // 没有下面这句, p[3] 实际上没有初始化
+    // 在工具链文件中, 使用 -fsanitize=memory 可以检查
 
     std::cout << "hello: " << p[3] << std::endl;
 
     // 如果没有 delete[] 就有内存泄露
+    // 在工具链文件中, 使用 -fsanitize=address 可以检查
     delete[] p;
     
     p = nullptr;
--- demo32/toolchain/ubuntu20_04-aarch64.cmake
+++ demo32/toolchain/ubuntu20_04-aarch64.cmake
@@ -12,8 +12,8 @@ set(CMAKE_SYSROOT /sdk/ubuntu20-aarch64)
 set(CMAKE_C_FLAGS_INIT "-fuse-ld=lld -Wno-unused-command-line-argument")
 set(CMAKE_CXX_FLAGS_INIT "-fuse-ld=lld -Wno-c++11-narrowing  -Wno-unused-command-line-argument")
 
-set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -fsanitize=address -rtlib=compiler-rt")
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}  -fsanitize=address -rtlib=compiler-rt")
+set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -fsanitize=memory -rtlib=compiler-rt")
+set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}  -fsanitize=memory -rtlib=compiler-rt")
 
 set(CMAKE_C_COMPILER clang-20)
 set(CMAKE_CXX_COMPILER clang++-20)
