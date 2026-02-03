2792e5dc0a4e44c63f121db167038b80e2b398e4
Tie.Wang <tie.wang@cyclone-robotics.com>
2026-02-03 13:09:25 +0800

    demo33

--- demo33-3/rebuild.bat
+++ demo33-3/rebuild.bat
@@ -7,6 +7,11 @@ CD /d %~dp0
 
 SET BUILD_DIR=build
 
+IF EXIST "%BUILD_DIR%" (
+    RMDIR /s /q "%BUILD_DIR%"
+)
+
+:: cmake -S src -B %BUILD_DIR% -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017"
 cmake -S src -B %BUILD_DIR% -DCMAKE_BUILD_TYPE=Release
 cmake --build %BUILD_DIR%
 ctest --test-dir %BUILD_DIR%
